8e44829e48deb14061f1fa333170ee31a459b678
imperative-publisher-3811 <mcasimirian@gmail.com>
2020-04-22 19:43:03 +0000

    Automatically backed up by Learn

--- hoisting.js
+++ hoisting.js
@@ -9,7 +9,7 @@ function callMe() {
 
 
 function crazy() {
-  var thisIsCrazy = function (){
+  var thisIsCrazy = function crazy(){
   // fix the code in here:
   console.log("hey!!!")
   thisIsCrazy();
